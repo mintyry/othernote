09085efdea6c9271f0d90c666a8c649760af2b87
mintyry <gitmintyry@gmail.com>
2023-11-09 18:08:04 -0800

    changed require to readfile

--- server.js
+++ server.js
@@ -30,56 +30,73 @@ app.use(express.static('public'));
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 })
+
 //route handler for notes made bc index.js specifies this path; leads sends notes.html(page) to client
 app.get('/api/notes', (req, res) => {
-    const existingNotes = require('./db/db.json');
-    res.json(existingNotes);
+    fs.readFile('./db/db.json', (err, data) => { //fs needs to take in string
+        if (err) {
+            console.log(err)
+        } else {
+            const existingNotes = JSON.parse(data);
+            res.json(existingNotes);
+        }
+    })
 })
 
 app.post('/api/notes', (req, res) => {
-   // require db.json so we have the content
-   const existingNotes = require('./db/db.json');
-   // take what client sends to us, specifically what's in title and text
-   const { title, text } = req.body;
+    // require db.json so we have the content
+    fs.readFile('./db/db.json', (err, data) => { //fs needs to take in string
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('Note saved!');
+            console.log(data);
+            console.log(typeof data);
+            const existingNotes = JSON.parse(data);
+            console.log(existingNotes);
+            // take what client sends to us, specifically what's in title and text
+            const { title, text } = req.body;
 
-   // If all the required properties are present
-   if (title && text) {
-       // Variable for the object we will save
-       const newNote = {
-           title,
-           text,
-           id: uuid(),
-       };
-       console.log(newNote);
+            // If all the required properties are present
+            if (title && text) {
+                // Variable for the object we will save
+                const newNote = {
+                    title,
+                    text,
+                    id: uuid(),
+                };
+                console.log(newNote);
 
-       existingNotes.push(newNote) //body is baked in; part of http requests; one of the properties of req for POST
-       console.log(existingNotes);
-       fs.writeFile('./db/db.json', JSON.stringify(existingNotes, null, 2), err => { //fs needs to take in string
-           if (err) {
-               console.log(err)
-           } else {
-               console.log('Note saved!');
-               res.status(201).end(); //similar to json, send, sendFile; let front end know you dont have to keep waiting
-               //route handlers always have to have response; knew to use end bc connection needs to end for functions to run
-           }
-       })
-   }
+                existingNotes.push(newNote) //body is baked in; part of http requests; one of the properties of req for POST
+                console.log(existingNotes);
+                fs.writeFile('./db/db.json', JSON.stringify(existingNotes, null, 2), err => { //fs needs to take in string
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log('Note saved!');
+                        res.status(201).end(); //similar to json, send, sendFile; let front end know you dont have to keep waiting
+                        //route handlers always have to have response; knew to use end bc connection needs to end for functions to run
+                    }
+                })
+            }
+        }
+    });
 })
 
 
 app.delete('/api/notes/:id', (req, res) => {
     // console.log(req.params.id);
     // const existingNotes = require('./db/db.json');
-   
+
     const existingNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
     // console.log(existingNotes, 'existing notes');
     const notesAfterDeletion = existingNotes.filter((notes) => notes.id !== req.params.id); // how does url even get the id to begin with?
     // console.log(notesAfterDeletion, 'notes after deletion');
     fs.writeFileSync('./db/db.json', JSON.stringify(notesAfterDeletion, null, 2)
     );
-   const updatedDb = fs.readFileSync('./db/db.json', 'utf-8');
-//    console.log(updatedDb, 'updated db');
-   res.json({ok:true});
+    const updatedDb = fs.readFileSync('./db/db.json', 'utf-8');
+    //    console.log(updatedDb, 'updated db');
+    res.json({ ok: true });
 });
 
 
